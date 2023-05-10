8a3e13fbc78332d9ac93b7e8bec3919d64f0680a
teofilovic-ivan <129052023+teofilovic-ivan@users.noreply.github.com>
2023-05-10 12:22:27 +0200

    Update test_script.sh

--- test_script.sh
+++ test_script.sh
@@ -1,8 +1,9 @@
-curl -s -d '{}' http://$apiID.execute-api.localhost.localstack.cloud:4566/local/requests
+requestID=$(curl -s -d '{}' http://$apiID.execute-api.localhost.localstack.cloud:4566/local/requests | jq -r .requestID);
+echo $requestID;
 for i in 1 2 3 4 5 6 7 8 9 10; 
 do 
     echo "Polling for processing result to appear in s3://archive-bucket/..."; 
-    awslocal s3 ls s3://archive-bucket/ | grep $$requestID && exit; 
+    awslocal s3 ls s3://archive-bucket/ | grep $requestID && exit; 
     sleep 7; 
 done
-awslocal dynamodb scan --table-name appRequests
+awslocal dynamodb scan --table-name appRequests;
